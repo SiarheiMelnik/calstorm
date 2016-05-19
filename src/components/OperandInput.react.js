3ce8270fd85a60822cf3fde2c24b545d9ad97565
Siarhei Melnik <siarhei.m.a@gmail.com>
2016-05-20 00:51:39 +0300

    feat: reducer

--- src/components/OperandInput.react.js
+++ src/components/OperandInput.react.js
@@ -1,14 +1,21 @@
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Row, Col, PageHeader, Form,
   FormGroup,
   FormControl, DropdownButton, MenuItem
 } from 'react-bootstrap';
+import * as operationActions from '../actions/operation';
+import { connect } from 'react-redux';
 
 class OperandInput extends Component {
-  onSelect = (obj) => {
-    console.log('SELECT', obj);
+  static propTypes = {
+    addOperation: PropTypes.func.isRequired,
   }
+
+  onSelect = (obj) => {
+
+  }
+
   render() {
     return (
       <Row>
@@ -16,11 +23,11 @@ class OperandInput extends Component {
         <Col md={12}>
           <Form inline>
             <FormGroup controlId="formOperand1">
-              <FormControl type="number" placeholder="Operand1" />
+              <FormControl ref="input_one" type="number" placeholder="Operand1" />
             </FormGroup>
             {' '}
             <FormGroup controlId="formOperand2">
-              <FormControl type="number" placeholder="Operand2" />
+              <FormControl ref="input_two" type="number" placeholder="Operand2" />
             </FormGroup>
             {' '}
             <DropdownButton title={"Calculate"} onSelect={this.onSelect} id={3}>
@@ -35,4 +42,4 @@ class OperandInput extends Component {
   }
 }
 
-export default OperandInput;
+export default connect(null, operationActions)(OperandInput);
--- src/reducers/operation.js
+++ src/reducers/operation.js
@@ -1,6 +1,43 @@
 
+import { ADD_OPERATION } from '../constants/ActionTypes';
+
+
+function executeOperation(state, { payload }) {
+  const { input, operand } = payload;
+  switch (operand) {
+    case '+':
+      return [
+        ...state,
+        {
+          operand,
+          result: input[0] + input[1]
+        }
+      ];
+    case '/':
+      return [
+        ...state,
+        {
+          operand,
+          result: input[0] / input[1]
+        }
+      ];
+    case '%':
+      return [
+        ...state,
+        {
+          operand,
+          result: input[0] % input[1]
+        }
+      ];
+    default:
+      return state;
+  }
+}
+
 export default function operationReducer(state = [], action) {
   switch (action.type) {
+    case ADD_OPERATION:
+      return executeOperation(state, action);
     default:
       return state;
   }
